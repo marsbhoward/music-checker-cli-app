8e14b2206380a36b4d21ae8a21b379092d9e949a
Mars Howard <marsbhoward@gmail.com>
2018-04-23 12:53:23 -0400

    added require/ added print streams

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,12 @@
 require_relative './movie.rb'
+require_relative './stream.rb'
+
 require 'nokogiri'
 require 'open-uri'
 
+
+
 class Scraper
-  def initialize
-    make_movies
-  end
 
   def get_netflix
     Nokogiri::HTML(open("http://collider.com/best-movies-on-netflix-streaming/"))
@@ -24,53 +25,79 @@ class Scraper
   end
 
   def make_movies
+
     self.get_netflix_movies.each do |post|
       movie = Movie.new
       movie.title = post.first[1]
-      movie.source = "Netflix"
+      movie.stream = "Netflix"
+      Stream.new (movie.stream)
     end
+
     self.get_hulu_movies.each do |post|
       movie = Movie.new
       movie.title = post.first[1]
-      movie.source = "Hulu"
+      movie.stream = "Hulu"
+      Stream.new (movie.stream)
     end
   end
 
   def print_movies
     self.make_movies
+    i = 0
     Movie.all.each do |movie|
-      puts Movie
+      puts "Movie #{i}"
       if movie.title
 
-        puts "Title: #{movie.title}"
-        puts "  Source: #{movie.source}"
+        puts "  Title: #{movie.title}"
+        puts "  stream: #{movie.stream}"
+        i = i + 1
       end
     end
+    Movie.all.clear
   end
 
 
   def print_netflix
     self.make_movies
+    i = 0
     Movie.all.each do |movie|
-      puts Movie
-      if movie.source = "Netflix"
+      puts "Movie #{i}"
+      if movie.stream == "Netflix"
 
-        puts "Title: #{movie.title}"
-        puts "  Source: #{movie.source}"
+        puts "  Title: #{movie.title}"
+        puts "  stream: #{movie.stream}"
+        i = i + 1
       end
     end
+    Movie.all.clear
   end
 
   def print_hulu
     self.make_movies
+    i = 0
     Movie.all.each do |movie|
-      puts Movie
-      if movie.source = "Hulu"
+      puts "Movie #{i}"
 
-        puts "Title: #{movie.title}"
-        puts "  Source: #{movie.source}"
+      if movie.stream == "Hulu"
+
+        puts "  Title: #{movie.title}"
+        puts "  stream: #{movie.stream}"
+        i = i + 1
       end
     end
+    Movie.all.clear
+  end
+
+  def print_streams
+    self.make_movies
+
+    streams = []
+    Movie.all.each do |movie|
+      if movie.title
+
+        streams << movie.stream unless streams.include?(movie.stream)
+      end
+    end
+    puts streams
   end
 end
-#Scraper.new.print_movies
