ab645428de5b162e7cdd291d33c3f8ad7da95f8d
Mars Howard <marsbhoward@gmail.com>
2018-04-23 12:49:47 -0400

    added list streams

--- lib/Command_Line_Interface.rb
+++ lib/Command_Line_Interface.rb
@@ -4,11 +4,14 @@ class CommandLineInterface
 	def call
 		input = ""
 
+
 		puts "Welcome to Movie Checker!"
 		while input != "exit"
+			puts "Main Menu"
 			puts "To see all movies currently streaming enter 'list movies'."
 			puts "To see all movies currently streaming on Netflix enter 'netflix'."
 			puts "To see all movies currently streaming on Hulu enter 'hulu'."
+			puts "to see the list of streaming services type 'list streams'"
 			puts "To quit enter 'exit'."
 			puts
 			puts "What would you like to do?"
@@ -23,7 +26,9 @@ class CommandLineInterface
 				when "netflix"
 					Scraper.new.print_netflix
 				when "hulu"
-					Scraper.new.print_movies
+					Scraper.new.print_hulu
+				when "list streams"
+					Scraper.new.print_streams
 			end
 		end
 	end
